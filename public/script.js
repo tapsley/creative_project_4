b49c97546b2a4e60e1bbfd1c3fd294943ee4e37d
tapsley <tyler.apsley@gmail.com>
2018-03-17 10:36:05 -0600

    fisrt

---
+++ public/script.js
@@ -0,0 +1,115 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    priority: 0,
+    items: [],
+    text: '',
+    show: 'all',
+    drag: {},
+	orderChange: false,
+	orderTarget: ""
+  },
+  created: function() {
+	  this.getItems();
+  }
+  computed: {
+    activeItems: function() {
+      return this.items.filter(function(item) {
+	return !item.completed;
+      });
+    },
+    filteredItems: function() {
+      if (this.show === 'active')
+	return this.items.filter(function(item) {
+	  return !item.completed;
+	});
+      if (this.show === 'completed')
+	return this.items.filter(function(item) {
+	  return item.completed;
+	});
+      return this.items;
+    },
+  },
+  methods: {
+    addItem: function() {
+		axios.post("/api/items", {
+			priority: this.priority,
+			text: this.text,
+			completed: false
+		}).then(response => {
+			this.priority = "1";
+			this.text = "";
+			this.getItems();
+			return true;
+		}).catch(err => {
+		});
+	},
+    completeItem: function(item) {
+      axios.put("/api/items/" + item.id, {
+		  text: item.text,
+		  completed: !item.completed,
+		  orderChange: false,
+	  }).then(response => {
+		  return true,
+	  }).catch(err => {
+	  });
+    },
+    deleteItem: function(item) {
+      axios.delete("/api/items/" + item.id).then(response => {
+		  this.getItems();
+		  return true;
+	  }).catch(err => {
+	  });
+    },
+	getItems: function() {
+		axios.get("/api/items").then(response => {
+			this.items = response.data;
+			return true;
+		}).catch( err => {
+		});
+	},
+	lowerPriority: function(item) {
+		if(Number(item.priority) < 3)
+			item.priority = Number(item.priority) + 1;
+	},
+	raisePriority: function(item) {
+		if(Number(item.priority) > 1)
+			item.priority = Number(item.priority) - 1;
+	},
+	sortByPriority: function() {
+		this.items.sort(function(a, b) {
+			return a.priority - b.priority;
+		});
+	},
+    showAll: function() {
+      this.show = 'all';
+    },
+    showActive: function() {
+      this.show = 'active';
+    },
+    showCompleted: function() {
+      this.show = 'completed';
+    },
+    deleteCompleted: function() {
+      this.items = this.items.filter(function(item) {
+	return !item.completed;
+      });
+    },
+    dragItem: function(item) {
+      this.drag = item;
+    },
+    dropItem: function(item) {
+      axios.put("/api/items/" + this.drag.id, {
+		  text: this.drag.text,
+		  completed: this.drag.completed,
+		  orderChange: true,
+		  orderTarget: item.id
+	  }).then( response => {
+		  this.getItems();
+		  return true;
+	  }).catch( err => {
+	  });
+    },
+  }
+});
+
---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static('public'));
+
+let items = [];
+let id = 0;
+
+app.get("/api/items", (req, res) => {
+	res.send(items);
+});
+
+app.post("/api/items", (req, res) => {
+	id = id + 1;
+	let item = {id:id, text:req.body.text, completed: req.body.completed, priority: req.body.priority};
+	items.push(item);
+	res.send(item);
+});
+
+app.put("/api/items/:id", (req, res) => {
+	let id = parseInt(req.params,id);
+	let itemsMap = items.map(item => {return item.id; });
+	let index = itemsMap.indexOf(id);
+	let item = items[index];
+	item.completed = req.body.completed;
+	item.text = req.body.text;
+	//handle drag and drop re-ordering
+	if(req.body.orderChange) {
+		let indexTarget = itemsMap.indexOf(req.body.orderTarget);
+		items.splice(index, 1);
+		items.splice(indexTarget, 0, item);
+	}
+	res.send(item);
+});
+
+app.delete("/api/items/:id", (req, res) => {
+	let id = parseInt(req.params.id);
+	let removeIndex = items.map(item => { return item.id; }).indexOf(id);
+	if(removeIndex === -1) {
+		res.status(404).send("Sorry, that item doesn't exist");
+		return;
+	}
+	items.splice(removeIndex, 1);
+	res.sendStatus(200);
+});
+
+
+app.listen(3003, () => console.log("Server listening on port 3000"))
