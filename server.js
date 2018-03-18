8385a9ca49e7c26f347b97c08145d0594842bdb3
tapsley <tyler.apsley@gmail.com>
2018-03-17 19:37:48 -0600

    lklklk

--- server.js
+++ server.js
@@ -49,4 +49,4 @@ app.delete("/api/items/:id", (req, res) => {
 });
 
 
-app.listen(3000, () => console.log("Server listening on port 3000"))
+app.listen(3003, () => console.log("Server listening on port 3003"))
